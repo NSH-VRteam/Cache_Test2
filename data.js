5fe1dfbdffe38997e6a81ad347adf385e01acddf
NSH-VRteam <vrteam.nsh@gmail.com>
2025-07-09 16:03:15 +0530

    tesat

---
+++ data.js
@@ -0,0 +1,232 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-foyer---me---1f",
+      "name": "Foyer - ME - 1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2402990018571955,
+          "pitch": 0.4333895148812612,
+          "rotation": 0,
+          "target": "1-study---m---1f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-study---m---1f",
+      "name": "Study - M - 1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.519651822484791,
+          "pitch": 0.4454657346564872,
+          "rotation": 0,
+          "target": "0-foyer---me---1f"
+        },
+        {
+          "yaw": 0.016895475864643572,
+          "pitch": 0.6404608807672574,
+          "rotation": 0,
+          "target": "2-study---1---1f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-study---1---1f",
+      "name": "Study - 1 - 1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.122480779862567,
+          "pitch": 0.7477225372356244,
+          "rotation": 0,
+          "target": "1-study---m---1f"
+        },
+        {
+          "yaw": -2.7694558175458432,
+          "pitch": 0.33847528398650617,
+          "rotation": 0,
+          "target": "0-foyer---me---1f"
+        },
+        {
+          "yaw": 0.03957272318877969,
+          "pitch": 0.9078301093208196,
+          "rotation": 0,
+          "target": "3-bath-2---m---1f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-bath-2---m---1f",
+      "name": "Bath 2 - M - 1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.007629333061817789,
+          "pitch": 0.601649044574625,
+          "rotation": 0,
+          "target": "1-study---m---1f"
+        },
+        {
+          "yaw": 0.31087107898293453,
+          "pitch": 0.284161708261907,
+          "rotation": 0,
+          "target": "0-foyer---me---1f"
+        },
+        {
+          "yaw": 1.6555386989236869,
+          "pitch": 0.7640997796545435,
+          "rotation": 0,
+          "target": "4-bath-2---1---1f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bath-2---1---1f",
+      "name": "Bath 2 - 1 - 1F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.104522195337424,
+          "pitch": 0.7438418380206144,
+          "rotation": 0,
+          "target": "3-bath-2---m---1f"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Cache Test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": 0,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  },
+  "version": "250703-1422"
+};
